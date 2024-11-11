16961f1e55f5d4ba727caf28086b5e112fb2eb5e
flutewizard100 <dorinamevans@gmail.com>
2024-11-11 16:27:06 -0500

    THis is good code it kinda worky

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
@@ -38,4 +38,6 @@ public class     Auto {
         frontRight.setPower(power);
 
     }
+
+
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FirstCompTele.java
@@ -0,0 +1,48 @@
+package org.firstinspires.ftc.teamcode;
+
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+
+@TeleOp
+public class FirstCompTele extends OpMode {
+    protected DcMotor backLeft;
+    protected DcMotor frontLeft;
+    protected DcMotor frontRight;
+    protected DcMotor backRight;
+
+    @Override
+    public void init() {
+        backLeft = hardwareMap.get(DcMotor.class, "back left");
+        frontLeft = hardwareMap.get(DcMotor.class, "front left");
+        frontRight = hardwareMap.get(DcMotor.class, "front right");
+        backRight = hardwareMap.get(DcMotor.class, "back right");
+
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+//        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    @Override
+    public void loop() {
+        double backLeftPower = gamepad1.left_stick_x + gamepad1.left_stick_y*13;
+        double frontLeftPower = -gamepad1.left_stick_x - gamepad1.left_stick_y;
+        double backRightPower = gamepad1.left_stick_x - gamepad1.left_stick_y;
+        double frontRightPower = -gamepad1.left_stick_x +  gamepad1.left_stick_y*13;
+
+        backLeft.setPower(backLeftPower);
+        frontLeft.setPower(frontLeftPower);
+        backRight.setPower(backRightPower);
+        frontRight.setPower(frontRightPower);
+
+
+    }
+}
