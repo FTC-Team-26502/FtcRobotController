6fe96ce11b137dfa9f3380b4cc176a96a2456cae
BRB-Robotics <singananyava@gmail.com>
2024-11-18 14:40:54 -0800

    Tele

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele.java
@@ -112,7 +112,7 @@ public class FirstCompTele extends LinearOpMode{
                 intake.setPosition = 0.5;
             }
 
-            
+
             // Show the elapsed game time and wheel power.
             telemetry.addData("Status", "Run Time: " + runtime.toString());
             telemetry.addData("Front left/Right", "%4.2f, %4.2f", leftFrontPower, rightFrontPower);
