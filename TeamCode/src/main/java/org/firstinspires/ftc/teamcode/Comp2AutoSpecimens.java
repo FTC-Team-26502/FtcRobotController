a6ed5938844c3d3ef4f123601129007b8983b686
Nora Evans <ananevans@gmail.com>
2025-01-24 16:10:28 -0500

    added trajectory sequence

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Comp2AutoSpecimens.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Comp2AutoSpecimens.java
@@ -5,38 +5,61 @@ import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
+
 @Autonomous
 public abstract class Comp2AutoSpecimens extends Comp2Specimens{
     
     public void loopOpMode() throws InterruptedException {
          //Build Trajectories:
-        Pose2d startPose = new Pose2d(15, -62, Math.toRadians(90));
+        Pose2d startPose = getPose2d(15, -62, Math.toRadians(90));
         driveTrain.setPoseEstimate(startPose);
-        Trajectory driveToBarStart = driveTrain.trajectoryBuilder(new Pose2d(), Math.toRadians(90))
-                .splineToConstantHeading(new Vector2d(0, -36), Math.toRadians(90))
-                .splineToConstantHeading(new Vector2d(-56, 13), Math.toRadians(90))
-                .splineToConstantHeading(new Vector2d(-50, 13), Math.toRadians(90))
+
+        TrajectorySequence driveToBarStart = driveTrain.trajectorySequenceBuilder(startPose)
+                .splineToConstantHeading(getVector2d(0, -36), getHeading(Math.toRadians(90)))
+                .splineToConstantHeading(getVector2d(-56, 13), getHeading(Math.toRadians(90)))
+                .splineToConstantHeading(getVector2d(-50, 13), getHeading(Math.toRadians(90)))
                 .build();
-        Trajectory pushSamplesPart1 = driveTrain.trajectoryBuilder(new Pose2d(), Math.toRadians(90))
-                .splineTo(new Vector2d(20, 50), Math.toRadians(90))
-                .splineToConstantHeading(new Vector2d(37, 50), Math.toRadians(90))
-                .splineToConstantHeading(new Vector2d(37,-10), Math.toRadians(90))
-                .splineToConstantHeading(new Vector2d(37, 50), Math.toRadians(90))
-//                push sample 1
-//                .splineToConstantHeading(new Vector2d(57,-10), Math.toRadians(90))
-//                .splineToConstantHeading(new Vector2d(57,-54), Math.toRadians(90))
-//                .splineToConstantHeading(new Vector2d(59,-10), Math.toRadians(90))
-//                .splineToConstantHeading(new Vector2d(62,-10), Math.toRadians(90))
-////                .forward(45)
-//                .splineToConstantHeading(new Vector2d(47,-55), Math.toRadians(90))
-                .build();
-        Trajectory part2 = driveTrain.trajectoryBuilder(new Pose2d(), Math.toRadians(90))
+
+        TrajectorySequence pushSamples = driveTrain.trajectorySequenceBuilder(
+                // TODO change this later if we do not drive to bar
+                driveToBarStart.end())
+                .splineTo(new Vector2d(0, -36), Math.toRadians(90))
+                .splineTo(new Vector2d(-56, 13), Math.toRadians(90))
+                .forward(6)
+                .forward(-10)
+                .splineTo(new Vector2d(37, -31), Math.toRadians(90))
+                .splineTo(new Vector2d(36,-10), Math.toRadians(90))
+                .strafeTo(new Vector2d(48,-10))
                 .strafeTo(new Vector2d(48,-54))
                 .strafeTo(new Vector2d(48,-10))
-//                .splineToConstantHeading(new Vector2d(54,-10), Math.toRadians(90))
-                .build();
+                .strafeTo(new Vector2d(57,-10))
+                .strafeTo(new Vector2d(57,-54))
+                .strafeTo(new Vector2d(59,-10))
+                .strafeTo(new Vector2d(62,-10))
+                .forward(45)
+                .strafeTo(new Vector2d(47,-55))
+                .forward(5)
+                .forward(-20)
+                .splineTo(new Vector2d(0, -56), Math.toRadians(270))
+                .forward(26)
+                .strafeTo(new Vector2d(47,-55))
+                .forward(-6)
+                .forward(6)
+                .strafeTo(new Vector2d(0, -30))
+                .strafeTo(new Vector2d(47,-55))
+                .forward(-6)
+                .forward(6)
+                .forward(-6)
+                .forward(6)
+                .strafeTo(new Vector2d(0, -30))
+                .strafeTo(new Vector2d(47,-55))
+                .forward(-6).build()
+    );
         waitForStart();
-        driveTrain.followTrajectory(pushSamplesPart1);
+        driveTrain.followTrajectorySequence(driveToBarStart);
+        // TODO hang specimen
+        driveTrain.followTrajectorySequence(pushSamples);
         sleep(3000);
         driveTrain.followTrajectory(part2);
 
