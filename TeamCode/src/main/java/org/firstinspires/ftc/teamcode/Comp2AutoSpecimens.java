f7c1221010d0d377122de0bdc809808ad977a694
Nora Evans <nora@Robotics4.brbad.local>
2025-01-24 13:13:49 -0500

    testing auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Comp2AutoSpecimens.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Comp2AutoSpecimens.java
@@ -1,4 +1,41 @@
 package org.firstinspires.ftc.teamcode;
 
-public class Comp2AutoSpecimens {
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous
+public class Comp2AutoSpecimens extends Comp2Specimens{
+    @Override
+    public void runOpMode() throws InterruptedException {
+         //Build Trajectorys:
+        Pose2d startPose = new Pose2d(15, -62, Math.toRadians(90));
+        driveTrain.setPoseEstimate(startPose);
+        Trajectory driveToBarStart = driveTrain.trajectoryBuilder(new Pose2d(), Math.toRadians(90))
+                .splineTo(new Vector2d(0, -36), Math.toRadians(90))
+                .splineTo(new Vector2d(-56, 13), Math.toRadians(90))
+                .forward(6)
+                .build();
+        Trajectory pushSamples = driveTrain.trajectoryBuilder(new Pose2d(), Math.toRadians(90))
+                .splineTo(new Vector2d(37, -31), Math.toRadians(90))
+                .splineTo(new Vector2d(36,-10), Math.toRadians(90))
+                .strafeTo(new Vector2d(48,-10))
+                .strafeTo(new Vector2d(48,-54))
+                .strafeTo(new Vector2d(48,-10))
+                .strafeTo(new Vector2d(57,-10))
+                .strafeTo(new Vector2d(57,-54))
+                .strafeTo(new Vector2d(59,-10))
+                .strafeTo(new Vector2d(62,-10))
+                .forward(45)
+                .strafeTo(new Vector2d(47,-55))
+                .build();
+        Trajectory driveToBar = driveTrain.trajectoryBuilder(new Pose2d(), Math.toRadians(90))
+                .strafeTo(new Vector2d(47,-55))
+                .forward(-6)
+                .build();
+        waitForStart();
+        driveTrain.followTrajectory(pushSamples);
+
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Comp2TeleOpRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Comp2TeleOpRed.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp(name = "TeleOp Red Specimens")
-public class Comp2TeleOpRedSpecimens extends  Comp2TeleOpSpecimens{
+public class Comp2TeleOpRed extends  Comp2TeleOpSpecimens {
     @Override
     public void runOpMode() {
         initOpMode(true);
